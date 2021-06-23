1a983bfc613ced7b29794a4577da8e5f8029e6f6
unknown <saimrafiq224@gmail.com>
2021-06-23 12:47:34 +0100

    Task 3 completed

--- Python_tasks.py
+++ Python_tasks.py
@@ -11,17 +11,18 @@
 # print("Welcome, " + Full_Name.strip())
 
 # Task 3
-age_rating = 13
+age_rating = input("What age rating is the movie? ")
+age_rating = str(age_rating)
 
 if age_rating == "universal":
     print("Everyone can watch!")
 elif age_rating == "pg":
     print("General viewing with some scenes unsuitable for young children")
-elif age_rating == 12:
+elif age_rating == "12":
     print("Films classified 12A and video works classified 12 contain material that is not generally suitable for children aged under 12. No one younger than 12 may see a 12A film in a cinema unless accompanied by an adult")
-elif age_rating == 15:
+elif age_rating == "15":
     print("No one younger than 15 may see a 15 film in a cinema")
-elif age_rating == 18:
+elif age_rating == "18":
     print("No one younger than 18 may see an 18 film in cinema")
 else:
     print("Incorrect age rating has been entered!")

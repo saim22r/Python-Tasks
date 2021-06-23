1d58ec3d1068b5bb1c66bd2e46303e5941ff5b41
unknown <saimrafiq224@gmail.com>
2021-06-23 16:56:53 +0100

    Tasks completed

---
+++ Task_6_FizzBuss.py
@@ -0,0 +1,11 @@
+# Task 6
+
+for i in range(100):
+    if i % 3 and i % 5 == int():
+        print("FizzBuzz")
+    elif i % 3 == int():
+        print("Fizz")
+    elif i % 5 == int():
+        print("Buzz")
+    else:
+        print(i)

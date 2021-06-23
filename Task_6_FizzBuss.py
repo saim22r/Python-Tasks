3f0dc348e5cb0a734852c82c8ee4cdb260067754
unknown <saimrafiq224@gmail.com>
2021-06-23 17:07:38 +0100

    Improvements

--- Task_6_FizzBuss.py
+++ Task_6_FizzBuss.py
@@ -1,11 +1,11 @@
 # Task 6
 
 for i in range(100):
-    if i % 3 and i % 5 == int():
+    if i % 3 and i % 5 == 0:
         print("FizzBuzz")
-    elif i % 3 == int():
+    elif i % 3 == 0:
         print("Fizz")
-    elif i % 5 == int():
+    elif i % 5 == 0:
         print("Buzz")
     else:
         print(i)

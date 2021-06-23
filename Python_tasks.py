c987eee045a0b1459d1ac08cd73e8d307e5a3882
unknown <saimrafiq224@gmail.com>
2021-06-23 12:07:49 +0100

    Tasks completed

---
+++ Python_tasks.py
@@ -0,0 +1,9 @@
+name = "name"
+name = input("what is your first name? ")
+first_name = name.capitalize()
+print("Hello, "+ first_name)
+
+full_name = "full_name"
+full_name = input("What is your full name including middle names? ")
+Full_Name = full_name.title()
+print("Welcome, " + Full_Name)
